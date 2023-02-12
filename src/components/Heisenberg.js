8d68e3c75057b1a20a5e62a637416ef9b6f8b1be
Mena Wang <wang.mena@gmail.com>
2023-02-11 19:34:17 -0800

    heisenberg animation hehe

---
+++ src/components/Heisenberg.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { ReactComponent as Heisenberg } from "./resources/heisenberg.svg";
+import { ReactComponent as HeisenbergWave } from "./resources/heisenberg_wave_1.svg";
+import "../css/Heisenberg.css";
+
+function HeisenbergContainer() {
+  return <HeisenbergWave />;
+}
+
+export default HeisenbergContainer;
