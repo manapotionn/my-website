a56c3a404bd1b9d0844291bb2584a1d57006e858
Mena Wang <wang.mena@gmail.com>
2023-03-02 22:09:09 -0800

    handwriting font + sort implementation + more quotes added

--- src/components/commonplace-book/CommonPlaceBookIndex.js
+++ src/components/commonplace-book/CommonPlaceBookIndex.js
@@ -5,18 +5,22 @@ import "./CommonPlaceBookIndex.css";
 
 function CommonPlaceBookIndex() {
   const [tagDisplay, setTagDisplay] = useState("lyrics");
+  const [isAsc, setIsAsc] = useState("false");
   return (
     <div className="commonPlace-container">
       <div className="tagsNav">
-        {/* dynamically get list of tags */}
+        {/* dynamically get list of tags ? */}
         <TagsNav
           tags={["lyrics", "movie quotes", "book quotes"]}
           setTagDisplay={setTagDisplay}
         />
+        <div onClick={() => setIsAsc(!isAsc)}>
+          sort by date: {isAsc ? "asc" : "desc"}
+        </div>
       </div>
       <div className="posts-container">
         <div>
-          <Posts selectedTag={tagDisplay} />
+          <Posts selectedTag={tagDisplay} sortByDate={isAsc} />
         </div>
       </div>
       {/* <div>footer placeholder</div> */}
--- src/components/commonplace-book/Posts.js
+++ src/components/commonplace-book/Posts.js
@@ -1,27 +1,42 @@
 import postsData from "./data/data.json";
 import "./Posts.css";
 
-function Posts({ selectedTag }) {
+function Posts({ selectedTag, sortByDate }) {
+  const filtered_data = postsData.filter((obj) => {
+    return obj.tag === selectedTag;
+  });
+
+  function asc(a, b) {
+    return a.year - b.year;
+  }
+
+  function desc(a, b) {
+    return b.year - a.year;
+  }
+
+  if (sortByDate) {
+    filtered_data.sort(asc);
+  } else {
+    filtered_data.sort(desc);
+  }
+
   return (
     <div className="posts">
-      {postsData.map((item, index) => {
-        /* i can see this logic being bad when i wanna do pagination... might rethink*/
-        if (item.tag === selectedTag) {
-          let quotes = item.content.split("\\");
-          return (
-            <div className={"post_" + index + " post"}>
-              <div className={"content content" + index}>
-                {quotes.map((line, index2) => {
-                  return <div className={"line_" + index2}>{line}</div>;
-                })}
-                <div className={"source"}>
-                  <div>{"-- " + item.author}</div>
-                  <div>{item.source}</div>
-                </div>
+      {filtered_data.map((item, index) => {
+        let quotes = item.content.split("\\");
+        return (
+          <div className={"post_" + index + " post"}>
+            <div className={"content content" + index}>
+              {quotes.map((line, index2) => {
+                return <div className={"line_" + index2}>{line}</div>;
+              })}
+              <div className={"source"}>
+                <div>{"-- " + item.author}</div>
+                <div>{item.source}</div>
               </div>
             </div>
-          );
-        }
+          </div>
+        );
       })}
     </div>
   );
