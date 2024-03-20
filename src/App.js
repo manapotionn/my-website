b371f04c8ab2685209558e6b5834d1a463d72b82
Mena Wang <wang.mena@gmail.com>
2024-03-19 23:17:03 -0700

    sticky panel title bar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Panel from "./components/Panel";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
-import File from "./components/File";
 
 import EuroBets from "./components/euro-bets/EuroBets";
-import Popo from "./components/stuffed-animals/Popo";
 import { AppStateProvider } from "./store/AppState";
 import { AppReducer } from "./store/AppReducer";
 import me from "./me.jpeg";
@@ -56,61 +54,6 @@ function App() {
         </div>
       </div>
     </AppStateProvider>
-
-    // <div>
-    //   <div className="app">
-    //     <div className="flexbox-left">
-    //       <Panel title={title}>
-    //         <About title={title} />
-    //       </Panel>
-    //     </div>
-    //     <div className="flexbox-right">
-    //       <div className="flexbox-right-top">
-    //         <Panel
-    //           title="me.jpg"
-    //           content={<img src={me} width="100%" height="100%" alt="me" />}
-    //         />
-    //       </div>
-    //       <div className="flexbox-right-bottom">
-    //         <Panel
-    //           title="files"
-    //           content={
-    //             <div>
-    //               <File
-    //                 title="about.txt"
-    //                 setTitle={setTitle}
-    //                 selected={title === "about.txt"}
-    //               />
-    //               <File
-    //                 title="experience.txt"
-    //                 setTitle={setTitle}
-    //                 selected={title === "experience.txt"}
-    //               />
-    //               <File
-    //                 title="skills.txt"
-    //                 setTitle={setTitle}
-    //                 selected={title === "skills.txt"}
-    //               />
-    //             </div>
-    //           }
-    //         />
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <br />
-    //   <br />
-
-    // {/* <div className="full-screen">
-    //   <Panel
-    //     title="eurobets.png"
-    //     content={<EuroBets title="hello"></EuroBets>}
-    //   />
-    // </div> */}
-
-    // {/* <div className="full-screen">
-    //   <Popo />
-    // </div> */}
-    // </div>
   );
 }
 
